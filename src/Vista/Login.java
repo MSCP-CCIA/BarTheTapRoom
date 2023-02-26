933b915d511a0e4581838cec5a8464a921733a75
Manuel Castro <manuel.castro01@correo.usa.edu.co>
2023-02-26 17:34:45 -0500

    CREACION DE LAS VISTAS LOGIN Y VISTA BAR THE TAP ROOM

--- src/Vista/Login.java
+++ src/Vista/Login.java
@@ -1,4 +1,9 @@
 package Vista;
 
+import javax.swing.*;
+
 public class Login {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton entrarButton;
 }
--- src/Vista/vistaBarTheTapRoom.java
+++ src/Vista/vistaBarTheTapRoom.java
@@ -1,4 +1,28 @@
 package Vista;
 
+import javax.swing.*;
+
 public class vistaBarTheTapRoom {
+    private JButton CREARCLIENTEButton;
+    private JButton BUSCARButton;
+    private JButton MODIFICARButton;
+    private JButton BUSCARVENTAButton;
+    private JButton ADICUIONARVENTAButton;
+    private JButton COREGIRButton;
+    private JButton BUSCARButton1;
+    private JButton ADICIONARPRODUCTOButton;
+    private JTable table1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTextField textField7;
+    private JTextField textField8;
+    private JTextField textField9;
+    private JTextField textField10;
+    private JTextField textField11;
+    private JTextField textField12;
+    private JTextField textField13;
 }
